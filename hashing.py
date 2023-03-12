d0faeab1f5bf1ddd050156fd543dea38c0b33ade
Zynkah <ffgurl07@yahoo.com>
2023-03-11 19:46:40 -0800

    Hashing

--- hashing.py
+++ hashing.py
@@ -70,3 +70,21 @@ class HashTable:
 
     def __setitem__(self, key, data):
         self.put(key, data)
+
+H=HashTable()
+H[54]="cat"
+H[26]="dog"
+H[93]="lion"
+H[17]="tiger"
+H[77]="bird"
+H[31]="cow"
+H[44]="goat"
+H[55]="pig"
+H[20]="chicken"
+H.slots
+[77, 44, 55, 20, 26, 93, 17, None, None, 31, 54]
+H.data
+['bird', 'goat', 'pig', 'chicken', 'dog', 'lion',
+       'tiger', None, None, 'cow', 'cat']
+
+print(H[17])
